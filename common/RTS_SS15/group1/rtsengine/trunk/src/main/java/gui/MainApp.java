63fa79a689c538ad3e6b4b2f158cb3c71a9d2ff6
pg318770 <pg318770@ec070dd3-eafd-0310-a3a0-839bcdcf1bc8>
2015-05-14 21:57:16 +0000

    Boilerplate for GUI

---
+++ common/RTS_SS15/group1/rtsengine/trunk/src/main/java/gui/MainApp.java
@@ -0,0 +1,75 @@
+package gui;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("RTS");
+
+        initRootLayout();
+
+        showMainView();
+    }
+
+    /**
+     * Initializes the root layout.
+     */
+    public void initRootLayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            System.out.print(MainApp.class.getResource("").getPath());
+            loader.setLocation(MainApp.class.getResource("/view/RootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
+
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Shows the main view inside the root layout.
+     */
+    public void showMainView() {
+        try {
+            // Load main view.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("/view/MainView.fxml"));
+            AnchorPane main = (AnchorPane) loader.load();
+
+            // Set main window into the center of root layout.
+            rootLayout.setCenter(main);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Returns the main stage.
+     * @return
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
