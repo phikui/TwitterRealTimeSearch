6277aa684cd6651fbeeb5a0dd58e1e6c8a8bf4bd
pg318770 <pg318770@ec070dd3-eafd-0310-a3a0-839bcdcf1bc8>
2015-05-17 15:42:25 +0000

    added most of the GUI functionality now saves everything into ConfigurationObject displays error messages for wrong weight and ratio distributions

--- rtsengine/trunk/src/main/java/gui/MainApp.java
+++ rtsengine/trunk/src/main/java/gui/MainApp.java
@@ -3,10 +3,13 @@ package gui;
 import java.io.IOException;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
--- rtsengine/trunk/src/main/java/gui/MainAppController.java
+++ rtsengine/trunk/src/main/java/gui/MainAppController.java
@@ -1,12 +1,17 @@
 package gui;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
@@ -17,11 +22,15 @@ import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.event.EventType;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import gui.MainApp;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
 /**
  * Created by Guerki on 16/5/15.
@@ -29,13 +38,73 @@ import javafx.scene.layout.AnchorPane;
 public class MainAppController implements Initializable {
     @FXML
     Button start;
+    @FXML
+    ToggleButton streamTweets;
+    @FXML
+    ToggleButton streamQueries;
+    @FXML
+    ComboBox<String> indexType;
+    @FXML
+    TextField numberOfThreads;
+    @FXML
+    TextField numberOfTweets;
+    @FXML
+    TextField ratio;
+    @FXML
+    TextField wSignificance;
+    @FXML
+    TextField wSimilarity;
+    @FXML
+    TextField wFreshness;
 
-    @Override
     public void initialize(URL url, ResourceBundle rsrcs) {
         start.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                /**
+                 * Get all the information from the GUI
+                 */
+                int nNumberOfThreads = Integer.parseInt(numberOfThreads.getCharacters().toString());
+                int nNumberOfTweets = Integer.parseInt(numberOfTweets.getCharacters().toString());
+                int nRatio = Integer.parseInt(ratio.getCharacters().toString());
+                int nWSignificance = Integer.parseInt(wSignificance.getCharacters().toString());
+                int nWSimilarity = Integer.parseInt(wSimilarity.getCharacters().toString());
+                int nWFreshness = Integer.parseInt(wFreshness.getCharacters().toString());
+                String nIndexType = indexType.getSelectionModel().getSelectedItem().toString();
+                Boolean nStreamTweets = streamTweets.isSelected();
+                Boolean nStreamQueries = streamQueries.isSelected();
+
+                /**
+                 * Check if data is valid
+                 */
+                if (nWFreshness+nWSignificance+nWSimilarity != 100){
+                    Alert alert = new Alert(Alert.AlertType.WARNING);
+                    alert.setTitle("Wrong Weights Ratio");
+                    alert.setHeaderText(null);
+                    alert.setContentText("The sum of all the weights needs to equal 100%.");
+                    alert.showAndWait();
+                    return;
+                }
+                if (nRatio < 0 || nRatio > 100){
+                    Alert alert = new Alert(Alert.AlertType.WARNING);
+                    alert.setTitle("Wrong Ratio");
+                    alert.setHeaderText(null);
+                    alert.setContentText("The Ratio must be between 0% and 100%.");
+                    alert.showAndWait();
+                    return;
+                }
+
+                /**
+                 * Save all the parameters into the ConfigurationObject
+                 */
+                ConfigurationObject.setIndexType(nIndexType);
+                ConfigurationObject.setNumberOfThreads(nNumberOfThreads);
+                ConfigurationObject.setNumberOfTweets(nNumberOfTweets);
+                ConfigurationObject.setRatio(nRatio);
+                ConfigurationObject.setStreamQueries(nStreamQueries);
+                ConfigurationObject.setStreamTweets(nStreamTweets);
+                ConfigurationObject.setwFreshness(nWFreshness);
+                ConfigurationObject.setwSignificance(nWSignificance);
+                ConfigurationObject.setwSimilarity(nWSimilarity);
             }
         });
     }
