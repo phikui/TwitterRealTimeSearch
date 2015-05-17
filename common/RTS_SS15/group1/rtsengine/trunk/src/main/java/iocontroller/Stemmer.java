2a17ed8e2ddf051022cb72d052ac6098a22d36c7
ns269217 <ns269217@ec070dd3-eafd-0310-a3a0-839bcdcf1bc8>
2015-05-17 19:46:29 +0000

    code for Stemming the querry

--- common/RTS_SS15/group1/rtsengine/trunk/src/main/java/iocontroller/Stemmer.java
+++ common/RTS_SS15/group1/rtsengine/trunk/src/main/java/iocontroller/Stemmer.java
@@ -19,34 +19,37 @@ public class Stemmer {
     public static void init() {
 
     }
-
-
-    public static List<String> stem(String text) {
-        // creates a StanfordCoreNLP object, with POS tagging, lemmatization, NER, parsing, and coreference resolution
-        Properties props = new Properties();
-        props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref");
-        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
-
+    protected StanfordCoreNLP pipeline;
+    public Stemmer()
+    {
+        // Create StanfordCoreNLP object properties, with POS tagging(required for lemmatization), and lemmatization
+        Properties props;
+        props = new Properties();
+        props.put("annotators", "tokenize, ssplit, pos, lemma");
+        // StanfordCoreNLP loads a lot of models, so we only want to do this once per execution
+        this.pipeline = new StanfordCoreNLP(props);
+    }
+    public  List<String> stem(String text) {
         // create an empty Annotation just with the given text
         Annotation document = new Annotation(text);
         // run all Annotators on this text
-        pipeline.annotate(document);
+        this.pipeline.annotate(document);
         // these are all the sentences in this document
         // a CoreMap is essentially a Map that uses class objects as keys and has values with custom types
+        List<String> wordList = new LinkedList<String>();
+        // Iterate over all of the sentences found
         List<CoreMap> sentences = document.get(CoreAnnotations.SentencesAnnotation.class);
-        List<String> wordList=new ArrayList<String>();
-        for(CoreMap sentence: document.get(CoreAnnotations.SentencesAnnotation.class))
-        {
-            for(CoreLabel token: sentence.get(CoreAnnotations.TokensAnnotation.class)){
-                String word = token.get(CoreAnnotations.TextAnnotation.class);
-                String lemma = token.get(CoreAnnotations.LemmaAnnotation.class);
-                wordList.add(lemma);
+        for (CoreMap sentence : sentences) {
+            // Iterate over all tokens in a sentence
+            for (CoreLabel token : sentence.get(CoreAnnotations.TokensAnnotation.class)) {
+                // Retrieve and add the lemma for each word into the list of lemmas
+                wordList.add(token.get(CoreAnnotations.LemmaAnnotation.class));
             }
         }
-        return wordList;
-        //TODO is to remove special symbols
-    }
 
+        return wordList;
+    }
+    //TODO is to remove special symbols
     public static List<String> trivial_stem(String text) {
         List<String> terms = new LinkedList<String>();
 
