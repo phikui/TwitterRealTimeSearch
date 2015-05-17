89d6726deef18f0b3738df38321f946fe8cb2c00
ns269217 <ns269217@ec070dd3-eafd-0310-a3a0-839bcdcf1bc8>
2015-05-17 14:16:57 +0000

    code for Stemming the querry

--- rtsengine/trunk/src/main/java/iocontroller/Stemmer.java
+++ rtsengine/trunk/src/main/java/iocontroller/Stemmer.java
@@ -1,14 +1,41 @@
 package iocontroller;
 
-import java.util.List;
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.util.CoreMap;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
 /**
  * Created by phil on 17.05.2015.
  */
 public class Stemmer {
 
     public static List<String> stem(String text) {
-        //TODO
-        return null;
+
+        // creates a StanfordCoreNLP object, with POS tagging, lemmatization, NER, parsing, and coreference resolution
+        Properties props = new Properties();
+        props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref");
+        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+        // create an empty Annotation just with the given text
+        Annotation document = new Annotation(text);
+        // run all Annotators on this text
+        pipeline.annotate(document);
+        // these are all the sentences in this document
+        // a CoreMap is essentially a Map that uses class objects as keys and has values with custom types
+        List<CoreMap> sentences = document.get(CoreAnnotations.SentencesAnnotation.class);
+        List<String> wordList=new ArrayList<String>();
+        for(CoreMap sentence: document.get(CoreAnnotations.SentencesAnnotation.class))
+        {
+            for(CoreLabel token: sentence.get(CoreAnnotations.TokensAnnotation.class)){
+                String word = token.get(CoreAnnotations.TextAnnotation.class);
+                String lemma = token.get(CoreAnnotations.LemmaAnnotation.class);
+                wordList.add(lemma);
+            }
+        }
+        return wordList;
     }
 }
