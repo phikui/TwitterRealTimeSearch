2c0a1a91054fd050e4368ece32330bedd72b0757
nh931768 <nh931768@ec070dd3-eafd-0310-a3a0-839bcdcf1bc8>
2015-05-23 15:36:59 +0000

    Refactoring of PreprocessorRawObject

--- common/RTS_SS15/group1/rtsengine/trunk/src/main/java/iocontroller/preprocessor/PreprocessorRawObject.java
+++ common/RTS_SS15/group1/rtsengine/trunk/src/main/java/iocontroller/preprocessor/PreprocessorRawObject.java
@@ -3,6 +3,7 @@ package iocontroller.preprocessor;
 import iocontroller.IOController;
 import model.TransportObject;
 import model.TweetObject;
+import sun.plugin2.message.transport.Transport;
 
 import java.util.Date;
 import java.util.List;
@@ -12,80 +13,29 @@ import java.util.concurrent.Callable;
  * Created by phil on 16.05.15.
  */
 public class PreprocessorRawObject implements Callable<TransportObject> {
-    private final Date timestamp;
-
-    private final int k;
-
-    // for determining if the object is a query or not
-    private final boolean isQuery;
-
-
-    //The tweet Content if it is a tweet
-    private final TweetObject tweet;
-
-
-    //The query if it is a query
-    private final String query;
+    private TransportObject transportObject;
 
     //Tweet constructor
-    public PreprocessorRawObject(TweetObject tweet) {
-        isQuery = false;
-        this.tweet = tweet;
-        timestamp = tweet.getTimestamp();
+    public PreprocessorRawObject(TransportObject transportObject) {
+        this.transportObject = transportObject;
 
-        //non relevant fields
-        k = 0;
-        query = null;
-
-    }
-
-    //query constructor
-    public PreprocessorRawObject(String queryString, int k, Date timestamp) {
-        isQuery = true;
-        query = queryString;
-        this.k = k;
-        this.timestamp = timestamp;
-
-        //non relevant fields
-        tweet = null;
     }
 
 
     //Code for prepossessing
     public TransportObject call() throws Exception {
-        TransportObject result;
         //TODO change trivial stem
         //distinguish between query and tweet
-        if (!isQuery) {
-            result = new TransportObject(tweet);
-            List<String> stems;
-            if (IOController.useStandfordStemmer) {
-                stems = IOController.stemmer.get().stem(tweet.getText());
-            } else {
-                stems = Stemmer.trivial_stem(tweet.getText());
-            }
-            result.setTerms(stems);
 
-
+        List<String> stems;
+        if (IOController.useStandfordStemmer) {
+            stems = IOController.stemmer.get().stem(transportObject.getText());
         } else {
-            result = new TransportObject(query, timestamp, k);
-            List<String> stems;
-            if (IOController.useStandfordStemmer) {
-                stems = IOController.stemmer.get().stem(tweet.getText());
-            } else {
-                stems = Stemmer.trivial_stem(query);
-            }
-            result.setTerms(stems);
-
-
+            stems = Stemmer.trivial_stem(transportObject.getText());
         }
+        transportObject.setTerms(stems);
 
-
-        return result;
-    }
-
-    public TweetObject getTweet() {
-        return tweet;
+        return transportObject;
     }
 
 }
