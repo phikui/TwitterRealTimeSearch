2402dfb231c7ab1d854cf5b338b75cb813cb7966
pk121860 <pk121860@ec070dd3-eafd-0310-a3a0-839bcdcf1bc8>
2015-06-10 17:16:59 +0000

    some comments

--- rtsengine/trunk/src/main/java/iocontroller/IOController.java
+++ rtsengine/trunk/src/main/java/iocontroller/IOController.java
@@ -16,6 +16,11 @@ import static java.lang.Runtime.getRuntime;
 
 /**
  * Created by phil on 17.05.15.
+ *
+ *
+ * This is the main communication tool. It handles arriving tweets and queries and does
+ * the necessary calculations and insertions
+ *
  */
 public class IOController {
     public final static boolean useStandfordStemmer = true;
@@ -37,6 +42,14 @@ public class IOController {
 
     private String message;
 
+
+    /**
+     * Instantiates an IOController object
+     *
+     * @param numPreProcessors   The number of preprocessor threads to use
+     * @param numQueryProcessors The number of queryprocessor threads to use
+     * @param writerOutput       Toggle for debug printouts
+     */
     public IOController(int numPreProcessors, int numQueryProcessors, boolean writerOutput) {
         queueContainer = new QueueContainer();
         preProcessor = new PreprocessorMainThread(queueContainer, numPreProcessors);
@@ -51,21 +64,38 @@ public class IOController {
         guiThread.setDaemon(true);
     }
 
+    /**
+     * Instantiates an IOController object
+     *
+     * @param numPreProcessors The number of preprocessor threads to use
+     * @param numQueryProcessors The number of queryprocessor threads to use
+     */
     public IOController(int numPreProcessors, int numQueryProcessors) {
         this(numPreProcessors, numQueryProcessors, false);
     }
 
+    /**
+     * Instantiates an IOController object using the default values of cores of machine preprocessors
+     * and 1 queryprocessor and sets the debug outputs to false
+     *
+     *
+     */
     public IOController() {
         this(getRuntime().availableProcessors(), 1, false);
     }
 
+    /**
+     * Start all threads
+     */
     public void startAll() {
         writer.start();
         guiThread.start();
     }
 
 
-
+    /**
+     * Stop all running threads, it will finish all current preprocessing/writing and queryprocessing
+     */
     public void stopAll() {
         if (this.hasUnprocessedItems()) {
             System.out.println("Warning: there are unprocessed items");
@@ -75,6 +105,7 @@ public class IOController {
         guiThread.terminate();
         tweetcollector.stopCollecting();
     }
+
 
     public void collectTweets() {
         tweetcollector.startCollecting();
@@ -92,12 +123,20 @@ public class IOController {
         writer.terminate();
     }
 
+
+    /**
+     * Calls the join method of associated threads
+     */
     public void waitForTermination() throws InterruptedException {
         writer.join();
         guiThread.join();
     }
 
-
+    /**
+     * Add a transport object to the preprocessor queue. Can be tweet or query
+     *
+     * @param transportObject The transport object
+     */
     public void addTransportObject(TransportObject transportObject) {
         PreprocessorRawObject rawObject = new PreprocessorRawObject(transportObject);
         preProcessor.addElement(rawObject);
