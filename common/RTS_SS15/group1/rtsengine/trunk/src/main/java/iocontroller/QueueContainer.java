16923e4568ec014aba3bc52de270c42e1a3a3043
pk121860 <pk121860@ec070dd3-eafd-0310-a3a0-839bcdcf1bc8>
2015-06-10 17:07:56 +0000

    changes to iocontroller

--- common/RTS_SS15/group1/rtsengine/trunk/src/main/java/iocontroller/QueueContainer.java
+++ common/RTS_SS15/group1/rtsengine/trunk/src/main/java/iocontroller/QueueContainer.java
@@ -1,6 +1,5 @@
 package iocontroller;
 
-import iocontroller.preprocessor.PreprocessorRawObject;
 import model.QueryReturnObject;
 import model.TransportObject;
 
@@ -12,12 +11,10 @@ import java.util.concurrent.LinkedBlockingQueue;
  * Created by phil on 16.05.15.
  */
 public class QueueContainer {
-    private final BlockingQueue<PreprocessorRawObject> preProcessorQueue;
     private final BlockingQueue<Future<TransportObject>> writerQueue;
     private final BlockingQueue<Future<QueryReturnObject>> queryOutputQueue;
 
     public QueueContainer() {
-        preProcessorQueue = new LinkedBlockingQueue<>();
         writerQueue = new LinkedBlockingQueue<>();
         queryOutputQueue = new LinkedBlockingQueue<>();
     }
@@ -26,9 +23,6 @@ public class QueueContainer {
         return writerQueue;
     }
 
-    public BlockingQueue<PreprocessorRawObject> getPreProcessorQueue() {
-        return preProcessorQueue;
-    }
 
     public BlockingQueue<Future<QueryReturnObject>> getQueryOutputQueue() {
         return queryOutputQueue;
