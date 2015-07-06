93c86cc69f1a46c76fe173c3172c12c17b455a30
pg318770 <pg318770@ec070dd3-eafd-0310-a3a0-839bcdcf1bc8>
2015-07-06 15:44:09 +0000

    included slope in location calculation

--- rtsengine/trunk/src/main/java/features/LocationFeature.java
+++ rtsengine/trunk/src/main/java/features/LocationFeature.java
@@ -16,56 +16,116 @@ import java.util.*;
  */
 public class LocationFeature extends FeatureBase {
 
-    private List<GeoLocation> geoLocList;
-    private Set<String> countrySet;
-    private List<Double> distanceList;
+    Set<String> countrySet = new HashSet<>();
 
     private static int numberOfTweets = 100000;
 
     public LocationFeature() {
-        this.geoLocList = new ArrayList<>();
-        this.countrySet = new HashSet<>();
-        this.distanceList = new ArrayList<>();
     }
 
     public double calculateLocationScore(String hashtag) {
 
         double locationScore;
-        double sumOfDistances;
+        double sumOfDistancs;
         int numberOfCountries;
+        double totalSlope;
+        List<GeoLocation> geoLocList = new ArrayList<>();
+        List<Double> distanceList = new ArrayList<>();
+        List<Double> intervalDistanceList = new ArrayList<>();
 
         // create new QueryObject and query AO for Hashtag
         this.createAndGetTweetList(hashtag, numberOfTweets);
 
         // iterate over tweetObjectList and gather GeoLocation information
-        populateGeoLocationList();
+        geoLocList = populateGeoLocationList(this.tweetObjectList);
 
         // iterate geoLocationList and create distanceList
-        createDistanceList();
+        distanceList = createDistanceList(geoLocList);
 
         // calculate the overall distance
-        sumOfDistances = calculateTotalDistance();
+        double sumOfDistances = calculateTotalDistance(distanceList);
+
+        // compute what distance the hashtag travelled per 5 minute intervals
+        intervalDistanceList = computeIntervalDistanceList();
+
+        totalSlope = computeSlope(intervalDistanceList);
 
         // function to determine feature score
         numberOfCountries = countrySet.size();
-        locationScore = calculateFeatureScore(sumOfDistances, numberOfCountries);
+        locationScore = calculateFeatureScore(sumOfDistances, numberOfCountries, totalSlope);
 
 
         return locationScore;
     }
 
-    private void populateGeoLocationList() {
+    private double computeSlope(List<Double> intervalDistanceList){
+        double sum = 0;
+        for (int i = 0; i<intervalDistanceList.size()-1; i++){
+            sum+= intervalDistanceList.get(i+1)-intervalDistanceList.get(i);
+        }
+        if(sum>0){
+            return sum;
+        }
+        else{
+            return 1;
+        }
+    }
 
-        System.out.println("GeoLocList Size: " + this.tweetObjectList.size());
+    private List<Double> computeIntervalDistanceList(){
+        Date timestamp1;
+        Date timestamp2;
+        int i = 0;
+        List<TweetObject> tweetList = new ArrayList<>();
+        TweetObject tweet1;
+        List<Double> intervalDistanceList = new ArrayList<>();
 
-        for (int i = 0; i < this.tweetObjectList.size(); i++) {
+        while(i <= this.tweetObjectList.size()-1){
+            tweet1 = this.tweetObjectList.get(i);
+            tweetList.add(tweet1);
+            timestamp1 = tweet1.getTimestamp();
+            if(i < this.tweetObjectList.size()-1){
+                timestamp2 = this.tweetObjectList.get(i+1).getTimestamp();
+            }
+            else{
+                break;
+            }
+            while(timeDiff(timestamp1, timestamp2) <= 5) {
+                i++;
+                tweetList.add(this.tweetObjectList.get(i));
+                if(i < this.tweetObjectList.size()-1){
+                    timestamp2 = this.tweetObjectList.get(i+1).getTimestamp();
+                }
+                else{
+                    break;
+                }
+            }
+            double sumOfDistances = calculateTotalDistance(createDistanceList(populateGeoLocationList(tweetList)));
+            intervalDistanceList.add(sumOfDistances);
+            i++;
+            tweetList.clear();
+        }
+        return intervalDistanceList;
+    }
+
+    private double timeDiff(Date ts1, Date ts2){
+        double result = ts2.getTime()/60000 - ts1.getTime()/60000;
+        return result;
+    }
+
+
+     private List<GeoLocation> populateGeoLocationList(List<TweetObject> tweetObjectList) {
+        List<GeoLocation> geoLocList = new ArrayList<>();
+
+        System.out.println("GeoLocList Size: " + tweetObjectList.size());
+
+        for (int i = 0; i < tweetObjectList.size(); i++) {
 
             // populate geoLocList
-            geoLocList.add(this.tweetObjectList.get(i).getGeoLocation());
+            geoLocList.add(tweetObjectList.get(i).getGeoLocation());
 
             // populate countrySet
-            if ((this.tweetObjectList.get(i).getPlace() != null) && (this.tweetObjectList.get(i).getPlace().getCountry() != null)) {
-                countrySet.add(this.tweetObjectList.get(i).getPlace().getCountry());
+            if ((tweetObjectList.get(i).getPlace() != null) && (tweetObjectList.get(i).getPlace().getCountry() != null)) {
+                countrySet.add(tweetObjectList.get(i).getPlace().getCountry());
             }
         }
 
@@ -73,11 +133,11 @@ public class LocationFeature extends FeatureBase {
         if (!(geoLocList.isEmpty()) && (countrySet.isEmpty())) {
             countrySet.add("undefined country");
         }
-
+        return geoLocList;
     }
 
 
-    private void createDistanceList() {
+    private List<Double> createDistanceList(List<GeoLocation> geoLocList) {
 
         double latitude_first;
         double longitude_first;
@@ -86,6 +146,8 @@ public class LocationFeature extends FeatureBase {
         double longitude_second;
 
         double distance;
+
+        List<Double> distanceList = new ArrayList<>();
 
         // compare each distance with each other and get distance in km between them
         for (int i = 0; i < geoLocList.size(); i++) {
@@ -113,9 +175,9 @@ public class LocationFeature extends FeatureBase {
 
             }
         }
+        return distanceList;
 
     }
-
 
     private double calculateLatLongDistance(double latitude_first, double longitude_first, double latitude_second, double longitude_second) {
         double theta = longitude_first - longitude_second;
@@ -147,7 +209,7 @@ public class LocationFeature extends FeatureBase {
     }
 
 
-    private double calculateTotalDistance() {
+    private double calculateTotalDistance(List<Double> distanceList){
         double totalDistance = 0.0;
 
         for (int i = 0; i < distanceList.size(); i++) {
@@ -159,10 +221,10 @@ public class LocationFeature extends FeatureBase {
     }
 
 
-    private double calculateFeatureScore(double sumOfDistances, int numberOfCountries) {
+    private double calculateFeatureScore(double sumOfDistances, int numberOfCountries, double totalSlope) {
         double featureScore;
 
-        featureScore = numberOfCountries * sumOfDistances;
+        featureScore = numberOfCountries * sumOfDistances * Math.exp(totalSlope);
 
         System.out.println("Feature Score: " + featureScore);
         return featureScore;
