993cd3477e8ea4c5e9322ea0ca4628f064d7f7dd
mk783166 <mk783166@ec070dd3-eafd-0310-a3a0-839bcdcf1bc8>
2015-07-10 14:28:35 +0000

    added how to get the popular hashtags

---
+++ rtsengine/trunk/src/main/java/features/MapDBLoad.java
@@ -0,0 +1,129 @@
+package features;
+
+import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Multiset;
+import com.google.common.collect.Multisets;
+import model.TweetObject;
+import org.mapdb.*;
+
+import java.io.*;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by Maik on 08.07.2015.
+ */
+public class MapDBLoad {
+
+    public static void createPopularHashtagSet() {
+
+        // TODO load MapDB file here
+        final DB db = DBMaker.newFileDB(new File("tweetdb"))
+                .closeOnJvmShutdown()
+                .make();
+
+        // for now assuming this format
+        final HTreeMap<Integer, TweetObject> tweetObjectMap = db.getHashMap("Tweets");
+
+        // set with hashtags
+        Multiset<String> hashTagSet = HashMultiset.create();
+
+        TweetObject currentTweetObject;
+        String currentTweetText;
+
+        // Pattern for recognizing hashtags
+        Pattern hashtagPattern = Pattern.compile("#(\\w+|\\W+)");
+
+        for (int index : tweetObjectMap.keySet()) {
+            currentTweetObject = tweetObjectMap.get(index);
+            currentTweetText = currentTweetObject.getText();
+
+            //currentTweetText = "#test #test #hallo #test #ggg #hallo #test nope #tt #test #i #d #just #write #some #stuff #i #i #can #just #i #more #words #to #have #and #sdf #a #b #c";
+
+            // match the pattern to the current tweet text and add hashtags to set
+            Matcher mat = hashtagPattern.matcher(currentTweetText);
+            while (mat.find()) {
+                hashTagSet.add(mat.group(1));
+            }
+
+        }
+
+        // write newly created hashtag set to a file
+        writeUniqueHashtagSetFile(hashTagSet);
+
+    }
+
+
+    /**
+     * ordered based on hashtag occurences in descending order
+     *
+     * @param uniqueHashTagSet
+     */
+    private static void writeUniqueHashtagSetFile(Multiset<String> uniqueHashTagSet) {
+
+        String newline = System.getProperty("line.separator");
+
+
+        BufferedWriter writer = null;
+        try {
+            File result = new File("UniqueHashtagSet");
+            writer = new BufferedWriter(new FileWriter(result, false));
+
+            for (String hashtag : Multisets.copyHighestCountFirst(uniqueHashTagSet).elementSet()) {
+                writer.write(hashtag);
+                writer.write(newline);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                assert writer != null;
+                writer.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public static List<String> loadHashtagFile() {
+        List<String> hashtagList = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader("UniqueHashtagSet"))) {
+
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                hashtagList.add(line);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //System.out.println(hashtagList);
+        return hashtagList;
+    }
+
+    public static List<String> getMostPopularHashtags(List<String> hashtagList) {
+        List<String> popularHashtagList = new ArrayList<>();
+
+        // top 20% rounded to int
+        int topK = hashtagList.size() / 5;
+
+        // current hashTagList element
+        String currentHashtag;
+
+        for (int i = 0; i < topK; i++) {
+            currentHashtag = hashtagList.get(i);
+            popularHashtagList.add(currentHashtag);
+        }
+
+        //System.out.println(popularHashtagList);
+        return popularHashtagList;
+    }
+
+
+}
